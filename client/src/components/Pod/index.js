a5e6487724685a16e2945ec0aea892f55b7bcf80
Kary Ma <karyma101@hotmail.com>
2019-01-23 23:01:31 -0800

    pod page content

--- client/src/components/Pod/index.js
+++ client/src/components/Pod/index.js
@@ -3,7 +3,38 @@ import React from 'react'
 export default function Pod() {
   return (
     <div>
-      <h1>Guide</h1>
+      <h3>Preception On Demand (Pod) System</h3>
+
+      <img src='' alt='' />
+
+      <h4>How it works</h4>
+
+      <div>
+        <h5>Hardware</h5>
+        <p>
+          The benefit of training with our portable system are athletes are faster over 10 yards in
+          any direction
+        </p>
+      </div>
+
+      <div>
+        <h5>Software</h5>
+        <p>
+          The software does two jobs it runs the hardware from a connected tablet and collects real
+          time data for coaches. The benefit is objective feedback for team performance in real time
+          and collects data for measuring the KPIs
+        </p>
+      </div>
+
+      <div>
+        <h5>Analytics</h5>
+        <p>
+          Coaches can automatically track and analyze players ability to make decision, how long it
+          takes them to notice an opportunity and how long it takes to start their movement.
+        </p>
+      </div>
+
+      <button>Contact Us</button>
     </div>
   )
 }
