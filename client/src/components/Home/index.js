95662bb6c989367cec214a8ff0ce1234843e4633
Kary Ma <karyma101@hotmail.com>
2019-01-23 20:19:55 -0800

    home page content

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -3,13 +3,69 @@ import React, { Fragment } from 'react'
 export default function Home() {
   return (
     <Fragment>
-      <div className='header'>
+      <header>
         <h1>Preception</h1>
         <h2>The Future of Training</h2>
         <p>We are a training tool for something that cannot be trained any other way...</p>
         <p>The Ability to act first.</p>
         <button>Contact Us</button>
-      </div>
+      </header>
+
+      <section>
+        <h3>The 4 Step Process</h3>
+        <div>
+          <span>Observe</span>
+          <span>Orient</span>
+          <span>Decide</span>
+          <span>Act</span>
+        </div>
+        <button>Learn More</button>
+      </section>
+
+      <section>
+        <h3>How It Works</h3>
+        <img src='' alt='' />
+        <button>Learn More</button>
+      </section>
+
+      <section>
+        <h3>Preception Performance Metrics</h3>
+        <ul>
+          <li>Awareness Rating %</li>
+          <li>Scans/Minute</li>
+          <li>Average Decision Making Speed</li>
+          <li>Complexity of Decisions</li>
+          <li>Awareness Balance</li>
+          <li>Orientation Rating</li>
+        </ul>
+        <button>Learn More</button>
+      </section>
+
+      <section>
+        <h3>Roadmap</h3>
+      </section>
+
+      <section>
+        <h3>Testimonials</h3>
+        <div>
+          <q>
+            If you're not trying to utilize technology in some form, you're going to be left
+            behind.. This is something I want to incorportate into every training [as a coach]
+          </q>
+          <p>
+            Kevin Harmse, Former Professional Soccer Player, Assistant - Simon Fraser University
+          </p>
+        </div>
+        <div>
+          <q>
+            If you're not trying to utilize technology in some form, you're going to be left
+            behind.. This is something I want to incorportate into every training [as a coach]
+          </q>
+          <p>
+            Kevin Harmse, Former Professional Soccer Player, Assistant - Simon Fraser University
+          </p>
+        </div>
+      </section>
     </Fragment>
   )
 }
