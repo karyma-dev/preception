f5e645a5bc881ddc526f163902562124c29d78ad
karyma101 <karyma101@hotmail.com>
2019-01-24 21:00:48 -0800

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,11 +4,24 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/process">4 Step Process</Link>
-      <Link to="/guide">How It Works</Link>
-      <Link to="/data">Data</Link>
-      <Link to="/contact">Contact</Link>
+      <img className="navbar-logo" src="navbar logo" alt="" />
+      <ul className="navbar-links">
+        <li className="navbar-links-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/process">4 Step Process</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/guide">How It Works</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/data">Data</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   )
 }
