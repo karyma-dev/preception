873cb37b64f23a8fc47905fd44784d27ae59106c
Wooj <wjchung@hotmail.ca>
2019-01-24 19:44:33 -0800

    added image and sized first part of home

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,7 +4,7 @@ import Contact from '../Contact'
 export default function Home() {
   return (
     <Fragment>
-      <header>
+      <header className="header">
         <h1>Preception</h1>
         <h2>The Future of Training</h2>
         <p>We are a training tool for something that cannot be trained any other way...</p>
