d3f5626e4e09e0ab244b41f53568cdb0dd4fed90
karyma101 <karyma101@hotmail.com>
2019-01-27 15:11:40 -0800

    contact

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Form from './Form'
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="container">
+      <Form />
     </div>
   )
 }
