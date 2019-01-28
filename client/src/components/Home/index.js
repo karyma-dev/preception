e0d8f449004f71d9211f49b398b663d3845121d2
Kary Ma <karyma101@hotmail.com>
2019-01-28 03:14:44 -0800

    added links to button

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import Contact from '../Contact'
 
 import ooda from '../../images/ooda.png'
@@ -12,41 +13,46 @@ import leigh from '../../images/profile/leigh.png'
 import jordan from '../../images/profile/jordan.png'
 import felipe from '../../images/profile/felipe.png'
 
-
 export default function Home() {
   return (
     <Fragment>
-      <header className="header">
-        <h1 className="header--primary-header">Preception</h1>
-        <h2 className="header--secondary-header">The Future of Training</h2>
+      <header className='header'>
+        <h1 className='header--primary-header'>Preception</h1>
+        <h2 className='header--secondary-header'>The Future of Training</h2>
       </header>
 
-      <section className="quotes-section">
-        <p className="quotes-section--primary-para">
+      <section className='quotes-section'>
+        <p className='quotes-section--primary-para'>
           We are a training tool for something that cannot be trained any other way...
         </p>
-        <p className="quotes-section--secondary-para">
+        <p className='quotes-section--secondary-para'>
           The Ability to act
           <br /> first
         </p>
-        <input type="button" className="btn quotes-section--button" value="Contact Us" />
+        <Link to='/contact'>
+          <input type='button' className='btn quotes-section--button' value='Contact Us' />
+        </Link>
       </section>
 
-      <section className="four-step-progress-section">
-        <h3 className="four-step-progress-section--header">The 4-Step Process</h3>
-        <img className="four-step-progress-section--img" src={ooda} alt="ooda model" />
-        <button className="btn four-step-progress-section--btn">Learn More</button>
+      <section className='four-step-progress-section'>
+        <h3 className='four-step-progress-section--header'>The 4-Step Process</h3>
+        <img className='four-step-progress-section--img' src={ooda} alt='ooda model' />
+        <Link to='/process'>
+          <button className='btn four-step-progress-section--btn'>Learn More</button>
+        </Link>
       </section>
 
-      <section className="how-it-works-section">
-        <h3 className="how-it-works-section--header">How It Works</h3>
-        <img className="how-it-works-section--img" src={pod} alt="preception on demand system" />
-        <button className="btn how-it-works-section--btn">Learn More</button>
+      <section className='how-it-works-section'>
+        <h3 className='how-it-works-section--header'>How It Works</h3>
+        <img className='how-it-works-section--img' src={pod} alt='preception on demand system' />
+        <Link to='/pod'>
+          <button className='btn how-it-works-section--btn'>Learn More</button>
+        </Link>
       </section>
 
-      <section className="performance-metrics-section">
-        <h3 className="performance-metrics-section--header">Preception Performance Metrics</h3>
-        <ul className="performance-metrics-section--list">
+      <section className='performance-metrics-section'>
+        <h3 className='performance-metrics-section--header'>Preception Performance Metrics</h3>
+        <ul className='performance-metrics-section--list'>
           <li>Awareness Rating %</li>
           <li>Scans/Minute</li>
           <li>Average Decision Making Speed</li>
@@ -54,120 +60,96 @@ export default function Home() {
           <li>Awareness Balance</li>
           <li>Orientation Rating</li>
         </ul>
-        <button className="btn performance-metrics-section--btn">Learn More</button>
+        <Link to='/data'>
+          <button className='btn performance-metrics-section--btn'>Learn More</button>
+        </Link>
       </section>
 
-      <section className="roadmap-section">
-
-        <h3 className="roadmap-section--header">Roadmap</h3>
-        <div className="line-orange" />
-        <img className="roadmap-section--img--1" src={roadmapWeb} alt="roadmap" />
-        <img className="roadmap-section--img--2" src={roadmapMob} alt="roadmap" />
-        <div className="line-orange" />
+      <section className='roadmap-section'>
+        <h3 className='roadmap-section--header'>Roadmap</h3>
+        <div className='line-orange' />
+        <img className='roadmap-section--img--1' src={roadmapWeb} alt='roadmap' />
+        <img className='roadmap-section--img--2' src={roadmapMob} alt='roadmap' />
+        <div className='line-orange' />
       </section>
 
-      <section className="testinomal-section">
-        <h3 className="testinomal-section--header">Testimonials</h3>
+      <section className='testinomal-section'>
+        <h3 className='testinomal-section--header'>Testimonials</h3>
 
-          <q className="testinomal-section--quote">
-            If you're not trying to utilize technology in some form, you're going to be left
-            behind.. This is something I want to incorportate into every training [as a coach]
-          </q>
-          <p className="testinomal-section--quoter">
-            Kevin Harmse, Former Professional Soccer Player, Assistant - Simon Fraser University
-          </p>
+        <q className='testinomal-section--quote'>
+          If you're not trying to utilize technology in some form, you're going to be left behind..
+          This is something I want to incorportate into every training [as a coach]
+        </q>
+        <p className='testinomal-section--quoter'>
+          Kevin Harmse, Former Professional Soccer Player, Assistant - Simon Fraser University
+        </p>
 
-          <q className="testinomal-section--quote">
-            The results became visually evident in short time. Players were displaying and repeating
-            the habits reinforced through the training (i.e. constant shoulder checking/scanning)
-            and then also started to more noticeably apply these habits into their other regular
-            training sessions and matches
-          </q>
-          <p className="testinomal-section--quoter">
-            Josh Smith, CSA <q>A</q> Licensed Coach
-          </p>
-
+        <q className='testinomal-section--quote'>
+          The results became visually evident in short time. Players were displaying and repeating
+          the habits reinforced through the training (i.e. constant shoulder checking/scanning) and
+          then also started to more noticeably apply these habits into their other regular training
+          sessions and matches
+        </q>
+        <p className='testinomal-section--quoter'>
+          Josh Smith, CSA <q>A</q> Licensed Coach
+        </p>
       </section>
 
-      <section className="team-section">
-        <div className="team-section--header">
+      <section className='team-section'>
+        <div className='team-section--header'>
           <h3>Preception Team</h3>
         </div>
 
-        <div className="team-section--members">
-
-        <div>
-            <img src={leigh} alt="Leigh Sembaluk" />
-            <span>
-              <h4>
-                Leigh Sembaluk
-              </h4>
-              <p>
-                Founder, CEO
-              </p>
-
-          </span>
-        <section className="team-section--buttons">
-          <button className="btn">Read Blogs</button>
-          <button className="btn">Purchase Book</button>
-        </section>
-        </div>
-        <div>
-          <section className="team-section--buttons-mobile">
-            <button className="btn">Read Blogs</button>
-            <button className="btn">Purchase Book</button>
-          </section>
-        </div>
+        <div className='team-section--members'>
           <div>
-            <img src={alex} alt="Alex Zucca" />
+            <img src={leigh} alt='Leigh Sembaluk' />
             <span>
-              <h4>
-                Alex Zucca
-              </h4>
-              <p>
-                Data Analyst
-              </p>
+              <h4>Leigh Sembaluk</h4>
+              <p>Founder, CEO</p>
+            </span>
+            <section className='team-section--buttons'>
+              <button className='btn'>Read Blogs</button>
+              <button className='btn'>Purchase Book</button>
+            </section>
+          </div>
+          <div>
+            <section className='team-section--buttons-mobile'>
+              <button className='btn'>Read Blogs</button>
+              <button className='btn'>Purchase Book</button>
+            </section>
+          </div>
+          <div>
+            <img src={alex} alt='Alex Zucca' />
+            <span>
+              <h4>Alex Zucca</h4>
+              <p>Data Analyst</p>
             </span>
           </div>
 
           <div>
-            <img src={felipe} alt="Felipe Pardo" />
+            <img src={felipe} alt='Felipe Pardo' />
             <span>
-              <h4>
-                Felipe Pardo
-              </h4>
-              <p>
-                Data Analyst
-              </p>
+              <h4>Felipe Pardo</h4>
+              <p>Data Analyst</p>
             </span>
           </div>
 
           <div>
-            <img src={yuan} alt="Yuan Wang" />
+            <img src={yuan} alt='Yuan Wang' />
             <span>
-              <h4>
-                Yuan Wang
-              </h4>
-              <p>
-                Developer
-              </p>
+              <h4>Yuan Wang</h4>
+              <p>Developer</p>
             </span>
           </div>
 
           <div>
-            <img src={jordan} alt="Jordan Shimell" />
+            <img src={jordan} alt='Jordan Shimell' />
             <span>
-              <h4>
-                Jordan Shimell
-              </h4>
-                <p>
-                  Developer
-                </p>
+              <h4>Jordan Shimell</h4>
+              <p>Developer</p>
             </span>
           </div>
         </div>
-
-
       </section>
 
       <Contact />
--- client/src/components/Process/index.js
+++ client/src/components/Process/index.js
@@ -1,54 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ooda from '../../images/ooda.png'
 
 export default function Process() {
   return (
-    <div className="container">
-      <div className="process">
-        <h3 className="process-title">The 4 Step Process</h3>
+    <div className='container'>
+      <div className='process'>
+        <h3 className='process-title'>The 4 Step Process</h3>
 
-        <img className="process-image" src={ooda} alt="ooda model" />
+        <img className='process-image' src={ooda} alt='ooda model' />
 
-        <div className="process-summary">
+        <div className='process-summary'>
           The 4 step linear process outlines every action you make: Observe, orient, decide, act. It
           has been nearly impossible to train and measure the first three steps. The final step
           (act) is a component that is regularly measured; however Preception measures the first
           three steps.
         </div>
 
-        <div className="process-section">
-          <h6 className="process-section-header">Observe</h6>
-          <p className="process-section-summary">
+        <div className='process-section'>
+          <h6 className='process-section-header'>Observe</h6>
+          <p className='process-section-summary'>
             Through integration in real team training environments athletes are able to observe
             their surroundings quicker and more complete ensuring they have access to key
             information at all times
           </p>
         </div>
 
-        <div className="process-section">
-          <h6 className="process-section-header">Orient</h6>
-          <p className="process-section-summary">
+        <div className='process-section'>
+          <h6 className='process-section-header'>Orient</h6>
+          <p className='process-section-summary'>
             Where individual athletes fit within the context of the team with more precision and
             flexibility
           </p>
         </div>
 
-        <div className="process-section">
-          <h6 className="process-section-header">Decide</h6>
-          <p className="process-section-summary">
+        <div className='process-section'>
+          <h6 className='process-section-header'>Decide</h6>
+          <p className='process-section-summary'>
             Athletes make quicker decision compared to opponents. Decisions are made with complete
             information ensuring the correct action
           </p>
         </div>
 
-        <div className="process-section">
-          <h6 className="process-section-header">Act</h6>
-          <p className="process-section-summary">
+        <div className='process-section'>
+          <h6 className='process-section-header'>Act</h6>
+          <p className='process-section-summary'>
             The result of the first 3 steps ensures the correct action
           </p>
         </div>
 
-        <button className="btn">Contact Us</button>
+        <Link to='/contact'>
+          <button className='btn'>Contact Us</button>
+        </Link>
       </div>
     </div>
   )
