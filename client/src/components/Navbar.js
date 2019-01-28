d4d841af9a938d8b6b56cac89703580a1bd5dfe0
Kary Ma <karyma101@hotmail.com>
2019-01-27 23:07:58 -0800

    link on logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,29 +6,31 @@ import menu from '../images/nav/menu.svg'
 export default function Navbar() {
   return (
     <Fragment>
-      <div className="navbar">
-        <div className="navbar-toggler">
-          <img src={menu} alt="hamburger menu" />
+      <div className='navbar'>
+        <div className='navbar-toggler'>
+          <img src={menu} alt='hamburger menu' />
         </div>
 
-        <div className="navbar-flex-container">
-          <img className="navbar-logo" src={logo} alt="navbar logo" />
+        <div className='navbar-flex-container'>
+          <Link to='/'>
+            <img className='navbar-logo' src={logo} alt='navbar logo' />
+          </Link>
 
-          <ul className="navbar-links">
-            <li className="navbar-links-item">
-              <Link to="/">Home</Link>
+          <ul className='navbar-links'>
+            <li className='navbar-links-item'>
+              <Link to='/'>Home</Link>
             </li>
-            <li className="navbar-links-item">
-              <Link to="/process">4 Step Process</Link>
+            <li className='navbar-links-item'>
+              <Link to='/process'>4 Step Process</Link>
             </li>
-            <li className="navbar-links-item">
-              <Link to="/pod">How It Works</Link>
+            <li className='navbar-links-item'>
+              <Link to='/pod'>How It Works</Link>
             </li>
-            <li className="navbar-links-item">
-              <Link to="/data">Data</Link>
+            <li className='navbar-links-item'>
+              <Link to='/data'>Data</Link>
             </li>
-            <li className="navbar-links-item">
-              <Link to="/contact">Contact</Link>
+            <li className='navbar-links-item'>
+              <Link to='/contact'>Contact</Link>
             </li>
           </ul>
         </div>
