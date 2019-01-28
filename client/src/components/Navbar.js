d15e66c9c62cc49b7054813409c4b75b383cc273
karyma101 <karyma101@hotmail.com>
2019-01-27 17:13:34 -0800

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/logo/white.png'
+import menu from '../images/nav/menu.svg'
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <img className="navbar-logo" src={logo} alt="navbar logo" />
-      <ul className="navbar-links">
-        <li className="navbar-links-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/process">4 Step Process</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/pod">How It Works</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/data">Data</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <Fragment>
+      <div className="navbar">
+        <div className="navbar-toggler">
+          <img src={menu} alt="hamburger menu" />
+        </div>
+
+        <div className="navbar-flex-container">
+          <img className="navbar-logo" src={logo} alt="navbar logo" />
+
+          <ul className="navbar-links">
+            <li className="navbar-links-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="navbar-links-item">
+              <Link to="/process">4 Step Process</Link>
+            </li>
+            <li className="navbar-links-item">
+              <Link to="/pod">How It Works</Link>
+            </li>
+            <li className="navbar-links-item">
+              <Link to="/data">Data</Link>
+            </li>
+            <li className="navbar-links-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Fragment>
   )
 }
