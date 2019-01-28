191929d2f8a5e61b42a32e18369f3e17201e1cc5
Kary Ma <karyma101@hotmail.com>
2019-01-28 02:08:47 -0800

    navbar toggler

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,34 +3,62 @@ import { Link } from 'react-router-dom'
 import logo from '../images/logo/white.png'
 import menu from '../images/nav/menu.svg'
 
-export default class Navbar extends Component() {
+export default class Navbar extends Component {
+  state = {
+    hidden: true
+  }
+
+  onClick = () => {
+    this.setState({
+      hidden: !this.state.hidden
+    })
+  }
+
+  onNavigate = () => {
+    this.setState({
+      hidden: true
+    })
+  }
+
   render() {
+    let hiddenToggler = this.state.hidden ? 'navbar-links hidden' : 'navbar-links'
+
     return (
       <div className='navbar'>
         <div className='navbar-toggler'>
-          <img src={menu} alt='hamburger menu' />
+          <img src={menu} alt='hamburger menu' onClick={this.onClick} />
         </div>
 
         <div className='navbar-flex-container'>
           <Link to='/'>
-            <img className='navbar-logo' src={logo} alt='navbar logo' />
+            <img className='navbar-logo' src={logo} alt='navbar logo' onClick={this.onNavigate} />
           </Link>
 
-          <ul className='navbar-links'>
+          <ul className={hiddenToggler}>
             <li className='navbar-links-item'>
-              <Link to='/'>Home</Link>
+              <Link to='/' onClick={this.onNavigate}>
+                Home
+              </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/process'>4 Step Process</Link>
+              <Link to='/process' onClick={this.onNavigate}>
+                4 Step Process
+              </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/pod'>How It Works</Link>
+              <Link to='/pod' onClick={this.onNavigate}>
+                How It Works
+              </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/data'>Data</Link>
+              <Link to='/data' onClick={this.onNavigate}>
+                Data
+              </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/contact'>Contact</Link>
+              <Link to='/contact' onClick={this.onNavigate}>
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
