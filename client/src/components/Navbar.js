f8efb4b3d15ff50b3bcc5982ff555f7b3aa6b481
Kary Ma <karyma101@hotmail.com>
2019-01-28 02:21:12 -0800

    exit button on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/logo/white.png'
 import menu from '../images/nav/menu.svg'
+import exit from '../images/nav/exit.svg'
 
 export default class Navbar extends Component {
   state = {
@@ -14,7 +15,7 @@ export default class Navbar extends Component {
     })
   }
 
-  onNavigate = () => {
+  closeMenu = () => {
     this.setState({
       hidden: true
     })
@@ -31,32 +32,36 @@ export default class Navbar extends Component {
 
         <div className='navbar-flex-container'>
           <Link to='/'>
-            <img className='navbar-logo' src={logo} alt='navbar logo' onClick={this.onNavigate} />
+            <img className='navbar-logo' src={logo} alt='navbar logo' onClick={this.closeMenu} />
           </Link>
 
           <ul className={hiddenToggler}>
+            <li className='navbar-links-exit'>
+              <img src={exit} alt='x' width='20px' onClick={this.closeMenu} />
+            </li>
+
             <li className='navbar-links-item'>
-              <Link to='/' onClick={this.onNavigate}>
+              <Link to='/' onClick={this.closeMenu}>
                 Home
               </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/process' onClick={this.onNavigate}>
+              <Link to='/process' onClick={this.closeMenu}>
                 4 Step Process
               </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/pod' onClick={this.onNavigate}>
+              <Link to='/pod' onClick={this.closeMenu}>
                 How It Works
               </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/data' onClick={this.onNavigate}>
+              <Link to='/data' onClick={this.closeMenu}>
                 Data
               </Link>
             </li>
             <li className='navbar-links-item'>
-              <Link to='/contact' onClick={this.onNavigate}>
+              <Link to='/contact' onClick={this.closeMenu}>
                 Contact
               </Link>
             </li>
