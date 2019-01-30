134a37898808b267b286561aad0d11d268831d9e
karyma101 <karyma101@hotmail.com>
2019-01-30 00:19:36 -0800

    made navbar bigger

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -25,42 +25,42 @@ export default class Navbar extends Component {
     let hiddenToggler = this.state.hidden ? 'navbar-links hidden' : 'navbar-links'
 
     return (
-      <div className='navbar'>
-        <div className='navbar-toggler'>
-          <img src={menu} alt='hamburger menu' onClick={this.onClick} />
+      <div className="navbar">
+        <div className="navbar-toggler">
+          <img className="navbar-menu" src={menu} alt="hamburger menu" onClick={this.onClick} />
         </div>
 
-        <Link to='/'>
-          <img className='navbar-logo' src={logo} alt='navbar logo' onClick={this.closeMenu} />
+        <Link to="/">
+          <img className="navbar-logo" src={logo} alt="navbar logo" onClick={this.closeMenu} />
         </Link>
 
         <ul className={hiddenToggler}>
-          <li className='navbar-links-exit'>
-            <img src={exit} alt='x' width='20px' onClick={this.closeMenu} />
+          <li className="navbar-links-exit">
+            <img src={exit} alt="x" width="20px" onClick={this.closeMenu} />
           </li>
 
-          <li className='navbar-links-item'>
-            <Link to='/' onClick={this.closeMenu}>
+          <li className="navbar-links-item">
+            <Link to="/" onClick={this.closeMenu}>
               Home
             </Link>
           </li>
-          <li className='navbar-links-item'>
-            <Link to='/process' onClick={this.closeMenu}>
+          <li className="navbar-links-item">
+            <Link to="/process" onClick={this.closeMenu}>
               4 Step Process
             </Link>
           </li>
-          <li className='navbar-links-item'>
-            <Link to='/pod' onClick={this.closeMenu}>
+          <li className="navbar-links-item">
+            <Link to="/pod" onClick={this.closeMenu}>
               How It Works
             </Link>
           </li>
-          <li className='navbar-links-item'>
-            <Link to='/data' onClick={this.closeMenu}>
+          <li className="navbar-links-item">
+            <Link to="/data" onClick={this.closeMenu}>
               Data
             </Link>
           </li>
-          <li className='navbar-links-item'>
-            <Link to='/contact' onClick={this.closeMenu}>
+          <li className="navbar-links-item">
+            <Link to="/contact" onClick={this.closeMenu}>
               Contact
             </Link>
           </li>
