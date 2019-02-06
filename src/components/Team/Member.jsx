8550fcd285f71cb87adc57b123eb78875de71316
Wooj <wjchung@hotmail.ca>
2019-02-05 17:50:55 -0800

    added icons and stuff

--- src/components/Team/Member.jsx
+++ src/components/Team/Member.jsx
@@ -7,7 +7,11 @@ export const Member = ({ name, desc, image, position }) => (
     </div>
     <div className="Members-content">
       <h3>{name}</h3>
-      <p>{position}</p>
+      <p className="Members-content-position">{position}</p>
+      <div className="Members-content-icons">
+        <i class="fab fa-linkedin Members-content-icons-linkedin"></i>
+        <i class="fab fa-twitter-square Members-content-icons-twitter"></i>
+      </div>
       <p className="Members-content-description">{desc}</p>
     </div>
   </div>
