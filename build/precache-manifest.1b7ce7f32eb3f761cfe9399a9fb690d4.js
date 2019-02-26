6a1f246a40d75793c85de4800f032bc234af9188
karyma101 <karyma101@hotmail.com>
2019-02-26 12:52:38 -0800

    build

---
+++ build/precache-manifest.1b7ce7f32eb3f761cfe9399a9fb690d4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2ce7af8f31efe8b8bcc1755020c4ce",
+    "url": "/static/media/jordan.1e2ce7af.png"
+  },
+  {
+    "revision": "803b694d8cf3699f61e9",
+    "url": "/static/css/main.9ec1e8b6.chunk.css"
+  },
+  {
+    "revision": "26fc669cf3055c7674de",
+    "url": "/static/js/1.26fc669c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09e0f0ae9c97c2d92549b86f48fcecb4",
+    "url": "/static/media/white.09e0f0ae.png"
+  },
+  {
+    "revision": "b7a199139be91e57dbf0f96975d9205a",
+    "url": "/static/media/exit.b7a19913.svg"
+  },
+  {
+    "revision": "4bd9dfb83f60944ef61643ccdf6c7e1f",
+    "url": "/static/media/menu.4bd9dfb8.svg"
+  },
+  {
+    "revision": "589f4ac60d6090b56f907d9d3ebebdf8",
+    "url": "/static/media/pod.589f4ac6.png"
+  },
+  {
+    "revision": "234e741cf548e72cb1129ba25b83d567",
+    "url": "/static/media/cool-logo.234e741c.png"
+  },
+  {
+    "revision": "5c53ccd9856fae913d8cf6a19b97871f",
+    "url": "/static/media/arrow.5c53ccd9.svg"
+  },
+  {
+    "revision": "6d912b346f4eb1694332198d43dcbfbc",
+    "url": "/static/media/alex-zucca.6d912b34.png"
+  },
+  {
+    "revision": "4182377059bb498a408f295613953bfe",
+    "url": "/static/media/yuan-wang.41823770.png"
+  },
+  {
+    "revision": "004a3bf99e5879f7a85af1a139f1677c",
+    "url": "/static/media/leigh.004a3bf9.png"
+  },
+  {
+    "revision": "803b694d8cf3699f61e9",
+    "url": "/static/js/main.803b694d.chunk.js"
+  },
+  {
+    "revision": "79e27c0072a425d1f3ed0360f968fb37",
+    "url": "/static/media/artyom.79e27c00.png"
+  },
+  {
+    "revision": "2b5e64cd49ebb2040df306d7b6768743",
+    "url": "/static/media/ooda.2b5e64cd.png"
+  },
+  {
+    "revision": "bf638d644dc33132f01ccc017561e1b8",
+    "url": "/static/media/pod-alt.bf638d64.png"
+  },
+  {
+    "revision": "05f40e99838fb2c3ccc401628e147c48",
+    "url": "/static/media/Venera-900.05f40e99.otf"
+  },
+  {
+    "revision": "fddc467daf863255a49bb742d5421b6f",
+    "url": "/static/media/Venera-300.fddc467d.otf"
+  },
+  {
+    "revision": "ce762a6d33ea66040bc3ead82583c015",
+    "url": "/static/media/testimonial-background.ce762a6d.png"
+  },
+  {
+    "revision": "695f1fd7cbe515ca02e9a79c441bec31",
+    "url": "/static/media/stadium.695f1fd7.jpg"
+  },
+  {
+    "revision": "4de2aa431b68c025cc2beccc90728ed1",
+    "url": "/static/media/4-step-background.4de2aa43.png"
+  },
+  {
+    "revision": "2b431dc77f0964f946b693796c96033c",
+    "url": "/static/media/sidekick.2b431dc7.png"
+  },
+  {
+    "revision": "79381790b290ba478621215d86cc2d4a",
+    "url": "/static/media/navbar.79381790.png"
+  },
+  {
+    "revision": "f703ebc5fb53a8ee0a6b6b6348b62bdf",
+    "url": "/static/media/section-rugby-blank.f703ebc5.jpg"
+  },
+  {
+    "revision": "656eedd70a6d1a257109c35df9e09973",
+    "url": "/index.html"
+  }
+];
